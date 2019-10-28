3edc85aebdfe46d8769a83eb0327e873598330c3
Dencer <tdaddon@163.com>
2019-10-28 13:08:36 +0800

    refactor: update bin

---
+++ bin/wct.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * @File   : wct.js
+ * @Author : Dencer (tdaddon@163.com)
+ * @Link   : https://dengsir.github.io
+ * @Date   : 10/28/2019, 1:07:58 PM
+ */
+
+require("../dist/index");

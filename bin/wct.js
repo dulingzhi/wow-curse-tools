1cd720101d7b039bc86c1421da51469e06bbe275
Dencer <tdaddon@163.com>
2019-10-28 13:10:16 +0800

    chore: update bin

--- bin/wct.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-/**
- * @File   : wct.js
- * @Author : Dencer (tdaddon@163.com)
- * @Link   : https://dengsir.github.io
- * @Date   : 10/28/2019, 1:07:58 PM
- */
-
-require("../dist/index");

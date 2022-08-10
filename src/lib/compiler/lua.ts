bfb80a6bff215400c595d0dd1c03dd8cf7948030
Dencer <tdaddon@163.com>
2022-08-11 00:01:36 +0800

    update

--- src/lib/compiler/lua.ts
+++ src/lib/compiler/lua.ts
@@ -8,33 +8,77 @@
 import { gEnv } from '../env';
 import { Compiler } from './compiler';
 
-export class LuaCompiler implements Compiler {
-    compile(code: string) {
-        const eq = this.getCommentEqual(code);
+type ReplacerFunc = (eq: string, a1: string, ...args: string[]) => string;
+type ReplacerItem = { r: RegExp; f: ReplacerFunc };
+type ReplacerPair = [ReplacerItem, ReplacerItem];
 
-        return code
-            .replace(/--\s*@(?<!end-|non-)(\w+)@/g, (r, x) => (gEnv.checkCondition(x) ? r : `--[${eq}[@${x}@`))
-            .replace(/--\s*@end-(?<!non-)(\w+)@/g, (r, x) => (gEnv.checkCondition(x) ? r : `--@end-${x}@]${eq}]`))
-            .replace(/--\[=*\[@non-(\w+)@/g, (r, x) => (gEnv.checkCondition(x) ? r : `--@non-${x}@`))
-            .replace(/--@end-non-(\w+)@\]=*\]/g, (r, x) => (gEnv.checkCondition(x) ? r : `--@end-non-${x}@`))
-            .replace(/--\s*@non-(\w+)@/g, (r, x) => (!gEnv.checkCondition(x) ? r : `--[${eq}[@non-${x}@`))
-            .replace(/--\s*@end-non-(\w+)@(?!\])/, (r, x) => (!gEnv.checkCondition(x) ? r : `--@end-non-${x}@]${eq}]`))
-            .replace(/--\s*@build([><=]+)(\d+)@/g, (r, o, b) =>
-                gEnv.checkBuild(o, b) ? r : `--[${eq}[@build${o}${b}@`
-            )
-            .replace(/--\s*@end-build([><=]+)(\d+)@(?!\])/g, (r, o, b) =>
-                gEnv.checkBuild(o, b) ? r : `--@end-build${o}${b}@]${eq}]`
-            );
+export class LuaCompiler implements Compiler {
+    private commentEquals: Set<number>;
+    private lastEqualsLength = 0;
+    private replacers: ReplacerPair[] = [
+        [
+            {
+                r: /--\s*@(?<!end-|non-)(\w+)@/g,
+                f: (e, s, x) => (gEnv.checkCondition(x) ? s : `--[${e}[@${x}@`),
+            },
+            {
+                r: /--\s*@end-(?<!non-)(\w+)@/g,
+                f: (e, s, x) => (gEnv.checkCondition(x) ? s : `--@end-${x}@]${e}]`),
+            },
+        ],
+        [
+            {
+                r: /--\[=*\[@non-(\w+)@/g,
+                f: (_, s, x) => (gEnv.checkCondition(x) ? s : `--@non-${x}@`),
+            },
+            {
+                r: /--@end-non-(\w+)@\]=*\]/g,
+                f: (_, s, x) => (gEnv.checkCondition(x) ? s : `--@end-non-${x}@`),
+            },
+        ],
+        [
+            {
+                r: /--\s*@non-(\w+)@/g,
+                f: (e, s, x) => (!gEnv.checkCondition(x) ? s : `--[${e}[@non-${x}@`),
+            },
+            {
+                r: /--\s*@end-non-(\w+)@(?!\])/,
+                f: (e, s, x) => (!gEnv.checkCondition(x) ? s : `--@end-non-${x}@]${e}]`),
+            },
+        ],
+        [
+            {
+                r: /--\s*@build([><=]+)(\d+)@/g,
+                f: (e, s, o, b) => (gEnv.checkBuild(o, b) ? s : `--[${e}[@build${o}${b}@`),
+            },
+            {
+                r: /--\s*@end-build([><=]+)(\d+)@(?!\])/g,
+                f: (e, s, o, b) => (gEnv.checkBuild(o, b) ? s : `--@end-build${o}${b}@]${e}]`),
+            },
+        ],
+    ];
+
+    compile(code: string) {
+        this.prepareCommentEqual(code);
+
+        for (const item of this.replacers) {
+            const equal = this.generateCommentEqual();
+            for (const { r, f } of item) {
+                code = code.replace(r, (...args) => f(equal, ...args));
+            }
+        }
+        return code;
     }
 
-    protected getCommentEqual(code: string) {
+    protected prepareCommentEqual(code: string) {
         const m = code.match(/\[(=*)\[|\](=*)\]/g);
-        const exists = new Set(m ? m.map((x) => x.length - 2) : []);
+        this.commentEquals = new Set(m ? m.map((x) => x.length - 2) : []);
+    }
 
-        let length = 0;
-        while (exists.has(length)) {
-            length++;
+    protected generateCommentEqual() {
+        while (this.commentEquals.has(this.lastEqualsLength)) {
+            this.lastEqualsLength++;
         }
-        return '='.repeat(length);
+        return '='.repeat(this.lastEqualsLength);
     }
 }

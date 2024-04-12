aa54428809d62e1b80641a78d22891fa16f2e209
Dencer <tdaddon@163.com>
2024-04-12 19:09:17 +0800

    feat: add update Ace3

---
+++ webpack.config.update.js
@@ -0,0 +1,60 @@
+// @ts-check
+
+"use strict";
+
+const path = require("path");
+
+/** @type {import('webpack').Configuration} */
+const config = {
+    target: "node",
+
+    entry: {
+        index: "./src/actions/update.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, "actions/curse-update-action"),
+        filename: "[name].js",
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".js"],
+        modules: ["node_modules"],
+    },
+    module: {
+        unknownContextCritical: false,
+        exprContextCritical: false,
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.lua$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "raw-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    externals: [
+        function ({ request }, callback) {
+            if (request === "wasmoon") {
+                require("./copy")(module.exports.output.path);
+                return callback(null, "./wasmoon");
+            }
+            callback();
+        },
+    ],
+};
+
+module.exports = config;

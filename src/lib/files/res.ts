f032dbd2292aa59f3a912c299b45ef42d8bde0c6
Dencer <tdaddon@163.com>
2024-04-19 17:37:24 +0800

    refactor: resfilter

--- src/lib/files/res.ts
+++ src/lib/files/res.ts
@@ -5,46 +5,24 @@
  * @Date   : 10/29/2019, 3:27:46 PM
  */
 
-import * as path from 'path';
-import * as fs from 'fs-extra';
+import path = require('path');
+import { glob } from 'fast-glob';
 import { gEnv } from '../env';
 
 export class ResFilesFinder {
-    private _files = new Set<string>();
-    private exts = new Set(['.blp', '.tga', '.m2', ...((gEnv.env && gEnv.env.resFilters) || [])]);
-
-    private isResource(filePath: string) {
-        const baseName = path.basename(filePath);
-        if (/^license/i.test(baseName)) {
-            return true;
-        }
-        if (/^bindings\.xml$/i.test(baseName)) {
-            return true;
-        }
-        if (this.exts.has(path.extname(filePath).toLowerCase())) {
-            return true;
-        }
-        return false;
-    }
-
-    private async walk(folder: string) {
-        const files = (await fs.readdir(folder))
-            .filter((name) => !name.startsWith('.') && !/^node_modules$/i.test(name))
-            .map((name) => path.resolve(folder, name));
-
-        for (const file of files) {
-            const stat = await fs.stat(file);
-
-            if (stat.isDirectory()) {
-                await this.walk(file);
-            } else if (this.isResource(file)) {
-                this._files.add(file);
-            }
-        }
-    }
-
     async findFiles(folder: string) {
-        await this.walk(folder);
-        return this._files;
+        const files = await glob.async(
+            [
+                '**.blp',
+                '**.tga',
+                '**.m2',
+                'bindings.xml',
+                '**license*',
+                '**/license*',
+                ...((gEnv.env && gEnv.env.resFilters) || []),
+            ],
+            { cwd: folder, caseSensitiveMatch: false }
+        );
+        return files.map((p) => path.resolve(p));
     }
 }
